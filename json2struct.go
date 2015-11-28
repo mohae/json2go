d3bbec7ffb92e75b45da421bd1f06b71ae22f8d1
Joel Scoble <joel.scoble@outlook.com>
2015-11-28 21:22:14 +0000

    refactor & generate embedded structs

--- json2struct.go
+++ json2struct.go
@@ -2,10 +2,14 @@ package json2struct
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"sort"
 	"strings"
+	"sync"
+
+	"github.com/mohae/firkin/queue"
 )
 
 // stringValues is a slice of reflect.Value holding *reflect.StringValue.
@@ -17,49 +21,92 @@ func (sv stringValues) Swap(i, j int)      { sv[i], sv[j] = sv[j], sv[i] }
 func (sv stringValues) Less(i, j int) bool { return sv.get(i) < sv.get(j) }
 func (sv stringValues) get(i int) string   { return sv[i].String() }
 
-// structDef creates a struct definition of type <em>name</em> from the
-// marshaled JSON and returns it as a formatted string.
-func structDef(name string, data interface{}) string {
-	var buff bytes.Buffer
-	err := gen(data, &buff)
-	if err != nil {
-		return ""
-	}
-	return fmt.Sprintf("type %s struct {\n%s}\n", name, buff.String())
-}
-// gen generates a byte representation of the interface{} fields, which is
-// assumed to be the result of a json.Unmarshal,
-func gen(data interface{}, buff *bytes.Buffer) error {
-	// TODO should pointers to interface be supported or generate an error?
-	var datum reflect.Value
-	switch data.(type) {
-	case map[string]interface{}:
-		datum = reflect.ValueOf(data)
-	default:
-		return fmt.Errorf("%q not supported", reflect.TypeOf(data).Kind())
-	}
-	toStruct(datum, buff)
-	return nil
+type structDef struct {
+	name string
+	val reflect.Value
+	buff bytes.Buffer
 }
 
-// takes a map[string]interface and a buffer, and populates the buffer
-// with the struct def.
-func toStruct(datum reflect.Value, buff *bytes.Buffer) error {
-	var sv stringValues = datum.MapKeys()
-	sort.Sort(sv)
-	for _, key := range sv {
-		k, tag := getFieldName(key)
-		_, err := buff.WriteString(fmt.Sprintf("\t%s\t", k))
-		if err != nil {
-			return err
-		}
-		val := datum.MapIndex(key)
-		typ := getValueKind(val)
-		buff.WriteString(typ)
-		buff.WriteString(fmt.Sprintf(" `json:%q`", tag))
-		buff.WriteString("\n")
+func newStructDef(name string, val reflect.Value) structDef {
+	s := structDef{name: name, val: val}
+	s.buff.WriteString(fmt.Sprintf("type %s struct {\n", name))
+	return s
+}
+
+func (s *structDef) Bytes() []byte {
+	s.buff.WriteString("}\n")
+	return s.buff.Bytes()
+}
+
+// Gen unmarshals JSON-encoded data and returns its struct definition(s) using
+// the name as the struct's name.  If the JSON includes other maps, the field
+// will be an embedded struct, with that struct's definition also being
+// generated.  If an error occurs during unmarshalling of the data, it will
+// be returned.  If an error occurs while writing to the buffer, that error
+// will be returned.
+func Gen(name string, data []byte) ([]byte, error) {
+	if name == "" {
+		return nil, fmt.Errorf("no name")
 	}
-	return nil
+	// unmarshal the JSON-encoded data
+	var datum interface{}
+	err := json.Unmarshal(data, &datum)
+	if err != nil {
+		return nil, err
+	}
+
+	var buff bytes.Buffer
+	var wg sync.WaitGroup
+	q := queue.NewQ(2)
+	result := make(chan []byte)
+	// start the worker
+	// send initial work item
+	q.Enqueue(newStructDef(name, reflect.ValueOf(datum)))
+	go func() {
+		defineStruct(q, result, &wg)
+	}()
+	// collect the results until the resCh is closed
+	var i int
+	for {
+		i++
+		val, ok := <-result
+		if !ok {
+			break
+		}
+		// TODO handle error/short read
+		buff.Write(val)
+	}
+	return buff.Bytes(), nil
+}
+
+func defineStruct(q *queue.Queue, result chan []byte, wg *sync.WaitGroup) {
+	for {
+		if q.IsEmpty() {
+			break
+		}
+		tmp, ok := q.Dequeue()
+		if !ok {
+			break
+		}
+		s := tmp.(structDef)
+		var sv stringValues = s.val.MapKeys()
+		sort.Sort(sv)
+		for _, key := range sv {
+			k, tag := getFieldName(key)
+			val := s.val.MapIndex(key)
+			typ := getValueKind(val)
+			// maps are embedded structs
+			if typ == reflect.Map.String() {
+				tmp := newStructDef(k, val.Elem())
+				q.Enqueue(tmp)
+				s.buff.WriteString(fmt.Sprintf("\t%s `json:%q`\n", k, tag))
+				continue
+			}
+			s.buff.WriteString(fmt.Sprintf("\t%s %s `json:%q`\n", k, typ, tag))
+		}
+		result <- s.Bytes()
+	}
+	close(result)
 }
 
 func getValueKind(val reflect.Value) string {
@@ -80,6 +127,10 @@ func getValueKind(val reflect.Value) string {
 			return fmt.Sprintf("[]%s", reflect.Float64.String())
 		}
 		return fmt.Sprintf("[]%s", v.Type().Kind().String())
+	case reflect.Struct:
+		fmt.Println("struct not handled")
+		return ""
+
 	}
 	return val.Elem().Type().Kind().String()
 }
--- json2struct_test.go
+++ json2struct_test.go
@@ -1,8 +1,6 @@
 package json2struct
 
 import (
-	"bytes"
-	"encoding/json"
 	"testing"
 )
 
@@ -13,29 +11,15 @@ var basic = []byte(`{
 	"baz": 42.1,
 	"foo_bar": "frood"
 }`)
-var expectedBasicFields = "\tBar\tstring `json:\"bar\"`\n\tBaz\tfloat64 `json:\"baz\"`\n\tBiz\tint `json:\"biz\"`\n\tFoo\tstring `json:\"foo\"`\n\tFooBar\tstring `json:\"foo_bar\"`\n"
-var expectedBasicStruct = "type Basic struct {\n\tBar\tstring `json:\"bar\"`\n\tBaz\tfloat64 `json:\"baz\"`\n\tBiz\tint `json:\"biz\"`\n\tFoo\tstring `json:\"foo\"`\n\tFooBar\tstring `json:\"foo_bar\"`\n}\n"
+var expectedBasicStruct = "type Basic struct {\n\tBar string `json:\"bar\"`\n\tBaz float64 `json:\"baz\"`\n\tBiz int `json:\"biz\"`\n\tFoo string `json:\"foo\"`\n\tFooBar string `json:\"foo_bar\"`\n}\n"
 
 func TestBasicStruct(t *testing.T) {
- 	var tst interface{}
-	err := json.Unmarshal(basic, &tst)
+	def, err := Gen("Basic", basic)
 	if err != nil {
-		t.Errorf("unexpected error unmarshaling basic test data: %q", err)
-		return
+		t.Errorf("unexpected error: %s", err)
 	}
-	var buff bytes.Buffer
-	err = gen(tst, &buff)
-	if err != nil {
-		t.Errorf("unexpected error generating struct: %q", err)
-		return
-	}
-	if buff.String() != expectedBasicFields {
-		t.Errorf("expected:\n%q, got:\n%q", expectedBasicFields, buff.String())
-	}
-	buff.Reset()
-	s := structDef("Basic", tst)
-	if s != expectedBasicStruct {
-		t.Errorf("expected %q got %q", expectedBasicStruct, s)
+	if string(def) != expectedBasicStruct {
+		t.Errorf("expected %q got %q", expectedBasicStruct, string(def))
 	}
 }
 
@@ -64,28 +48,56 @@ var intermediate = []byte(`{
 	],
 	"date": "Fri Jan 23 13:02:46 +0000 2015"
 }`)
-var expectedIntermediateFields = "\tBools\t[]bool `json:\"bools\"`\n\tBot\tbool `json:\"bot\"`\n\tDate\tstring `json:\"date\"`\n\tFloats\t[]float64 `json:\"floats\"`\n\tId\tint `json:\"id\"`\n\tInts\t[]int `json:\"ints\"`\n\tName\tstring `json:\"name\"`\n\tQuotes\t[]string `json:\"quotes\"`\n"
-var expectedIntermediateStruct = "type Intermediate struct {\n\tBools\t[]bool `json:\"bools\"`\n\tBot\tbool `json:\"bot\"`\n\tDate\tstring `json:\"date\"`\n\tFloats\t[]float64 `json:\"floats\"`\n\tId\tint `json:\"id\"`\n\tInts\t[]int `json:\"ints\"`\n\tName\tstring `json:\"name\"`\n\tQuotes\t[]string `json:\"quotes\"`\n}\n"
+var expectedIntermediateStruct = "type Intermediate struct {\n\tBools []bool `json:\"bools\"`\n\tBot bool `json:\"bot\"`\n\tDate string `json:\"date\"`\n\tFloats []float64 `json:\"floats\"`\n\tId int `json:\"id\"`\n\tInts []int `json:\"ints\"`\n\tName string `json:\"name\"`\n\tQuotes []string `json:\"quotes\"`\n}\n"
 
 func TestIntermediateStruct(t *testing.T) {
- 	var tst interface{}
-	err := json.Unmarshal(intermediate, &tst)
+	def, err := Gen("Intermediate", intermediate)
 	if err != nil {
-		t.Errorf("unexpected error unmarshaling basic test data: %q", err)
-		return
+		t.Errorf("unexpected error: %s", err)
 	}
-	var buff bytes.Buffer
-	err = gen(tst, &buff)
+	if string(def) != expectedIntermediateStruct {
+		t.Errorf("expected %q got %q", expectedIntermediateStruct, string(def))
+	}
+}
+
+// widget example from json.org/example.html
+var widget = []byte(`{
+	"widget": {
+		"debug": "on",
+		"window": {
+			"title": "Sample Konfabulator Widget",
+        		"name": "main_window",
+        		"width": 500,
+        		"height": 500
+		},
+		"image": {
+        		"src": "Images/Sun.png",
+        		"name": "sun1",
+        		"hOffset": 250,
+        		"vOffset": 250,
+        		"alignment": "center"
+		},
+		"text": {
+			"data": "Click Here",
+        		"size": 36,
+        		"style": "bold",
+        		"name": "text1",
+        		"hOffset": 250,
+        		"vOffset": 100,
+        		"alignment": "center",
+        		"onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+		}
+	}
+}`)
+
+var expectedWidget = "type TestW struct {\n\tWidget `json:\"widget\"`\n}\ntype Widget struct {\n\tDebug string `json:\"debug\"`\n\tImage `json:\"image\"`\n\tText `json:\"text\"`\n\tWindow `json:\"window\"`\n}\ntype Image struct {\n\tAlignment string `json:\"alignment\"`\n\tHOffset int `json:\"hOffset\"`\n\tName string `json:\"name\"`\n\tSrc string `json:\"src\"`\n\tVOffset int `json:\"vOffset\"`\n}\ntype Text struct {\n\tAlignment string `json:\"alignment\"`\n\tData string `json:\"data\"`\n\tHOffset int `json:\"hOffset\"`\n\tName string `json:\"name\"`\n\tOnMouseUp string `json:\"onMouseUp\"`\n\tSize int `json:\"size\"`\n\tStyle string `json:\"style\"`\n\tVOffset int `json:\"vOffset\"`\n}\ntype Window struct {\n\tHeight int `json:\"height\"`\n\tName string `json:\"name\"`\n\tTitle string `json:\"title\"`\n\tWidth int `json:\"width\"`\n}\n"
+
+func TestWidget(t *testing.T) {
+	s, err := Gen("TestW", widget)
 	if err != nil {
-		t.Errorf("unexpected error generating struct: %q", err)
-		return
+		t.Errorf("unexpected error %q", err)
 	}
-	if buff.String() != expectedIntermediateFields {
-		t.Errorf("expected:\n%q, got:\n%q", expectedIntermediateFields, buff.String())
-	}
-	buff.Reset()
-	s := structDef("Intermediate", tst)
-	if s != expectedIntermediateStruct {
-		t.Errorf("expected %q got %q", expectedIntermediateStruct, s)
+	if string(s) != expectedWidget {
+		t.Errorf("expected:\n%q, got:\n%q", expectedWidget, string(s))
 	}
 }
